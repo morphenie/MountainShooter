08099c7e234d5bb08be556a464b311a45c618b9c
Camila <camylaallllvesss@hotmail.com>
2025-03-02 17:30:35 -0300

    Cr8ng asset folder Inserting all assets Creating the menu> bg, sound and fonts Creanting const file

--- code/Game.py
+++ code/Game.py
@@ -4,29 +4,21 @@
 import pygame
 
 from code.Menu import Menu
+from code.const import WIN_WIDTH, WIN_HEIGHT
 
 
 class Game:
     def __init__(self):  #construtor da class Game
         pygame.init()  # é necessário para iniciar o pacote do pygame
-        self.window = pygame.display.set_mode(size=(600, 480))  # criação de janela do jogo
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))  # criação de janela do jogo com a resolução da imagem
 
     def run(self):  #método run
 
-        # ---------- aqui foi copiado do main ---------
-#        print('Setup Start')
 
-#        print('Setup End')
-
-#        print('Loop Start')
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
 
 
-            # Check for all events
-            #for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:  # o evento do tipo "fechar janela"
-            #        pygame.quit()  # Close window
-            #        quit()  # End Pygame
+##
---
+++ code/const.py
@@ -0,0 +1,21 @@
+# Lista de Constantes
+
+# C
+
+# Constantes de cores
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+# Para fazer o print do menu
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+
+# essa constante irá atualizar os valores do tamanho da imagem do background
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
